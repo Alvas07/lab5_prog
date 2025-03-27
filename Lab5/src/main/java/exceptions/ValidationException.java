b5394c0ebaa83b07aa4049cd3eb079a455cba205
Владислав Дядев <vladislavdyadev@MacBook-Air-Vladislav-4.local>
2025-03-28 02:03:36 +0300

    Updated text in ValidationException

--- Lab5/src/main/java/exceptions/ValidationException.java
+++ Lab5/src/main/java/exceptions/ValidationException.java
@@ -2,6 +2,6 @@ package exceptions;
 
 public class ValidationException extends Exception {
     public ValidationException(String message) {
-        super(message);
+        super("Ошибка при валидации данных: " + message);
     }
 }
