092daa9b669ee9eb2a4f0a7e400cbd10ca4f890c
Владислав Дядев <vladislavdyadev@MacBook-Air-Vladislav-4.local>
2025-03-28 02:37:51 +0300

    Updated Validation methods

--- Lab5/src/main/java/utils/Validator.java
+++ Lab5/src/main/java/utils/Validator.java
@@ -6,80 +6,98 @@ import data.TicketType;
 import exceptions.ValidationException;
 
 public class Validator {
-    public static String validateName(String name) throws ValidationException {
-        if (name == null || name.trim().isEmpty()) {
+    public static void validateName(String arg) throws ValidationException {
+        if (arg == null || arg.trim().isEmpty()) {
             throw new ValidationException("Название билета не может быть пустым.");
         }
-        return name;
     }
 
-    public static Coordinates validateCoordinates(Coordinates coordinates) throws ValidationException {
+    public static void validateCoordinates(Coordinates coordinates) throws ValidationException {
         if (coordinates == null) {
             throw new ValidationException("Координаты не могут быть null.");
         }
-        return coordinates;
     }
 
-    public static float validatePrice(float price) throws ValidationException {
-        if (price <= 0) {
-            throw new ValidationException("Стоимость должна быть больше нуля.");
+    public static void validatePrice(String arg) throws ValidationException {
+        try {
+            float price = Float.parseFloat(arg);
+            if (price <= 0) {
+                throw new ValidationException("Стоимость должна быть больше нуля.");
+            }
+        } catch (NumberFormatException e) {
+            throw new ValidationException("Стоимость билета должна быть числом типа float.");
         }
-        return price;
     }
 
-    public static TicketType validateType(TicketType type) throws ValidationException {
-        if (type == null) {
-            throw new ValidationException("Тип билета не может быть null.");
+    public static void validateType(String arg) throws ValidationException {
+        try {
+            TicketType.valueOf(arg);
+        } catch (IllegalArgumentException e) {
+            throw new ValidationException("Неверный параметр. Выберите один из предложенных.");
         }
-        return type;
+
     }
 
-    public static Long validateCoordinatesY(Long y) throws ValidationException {
-        if (y == null || y > 332) {
-            throw new ValidationException("Координата Y не может быть больше 332 или null.");
+    public static void validateCoordinatesY(String arg) throws ValidationException {
+        try {
+            Long y = Long.valueOf(arg);
+            if (y > 332) {
+                throw new ValidationException("Координата Y не может быть больше 332 или null.");
+            }
+        } catch (NumberFormatException e) {
+            throw new ValidationException("Координата Y должна быть числом типа long.");
         }
-        return y;
     }
 
-    public static Long validateLocationX(Long x) throws ValidationException {
-        if (x == null) {
-            throw new ValidationException("Местоположение по X не может быть null.");
+    public static void validateLocationX(String arg) throws ValidationException {
+        try {
+            Long x = Long.valueOf(arg);
+        } catch (NumberFormatException e) {
+            throw new ValidationException("Местоположение по X должно быть числом типа long.");
         }
-        return x;
     }
 
-    public static Long validateLocationY(Long y) throws ValidationException {
-        if (y == null) {
-            throw new ValidationException("Местоположение по Y не может быть null.");
+    public static void validateLocationY(String arg) throws ValidationException {
+        try {
+            Long y = Long.valueOf(arg);
+        } catch (NumberFormatException e) {
+            throw new ValidationException("Местоположение по Y должно быть числом типа long.");
         }
-        return y;
     }
 
-    public static Integer validateLocationZ(Integer z) throws ValidationException {
-        if (z == null) {
-            throw new ValidationException("Местоположение по Z не может быть null.");
+    public static void validateLocationZ(String arg) throws ValidationException {
+        try {
+            Integer z = Integer.valueOf(arg);
+        } catch (NumberFormatException e) {
+            throw new ValidationException("Местоположение по Z должно быть числом типа long.");
         }
-        return z;
     }
 
-    public static Float validateHeight(Float height) throws ValidationException {
-        if (height == null || height <= 0) {
-            throw new ValidationException("Рост человека должен быть больше нуля и не быть null.");
+    public static void validateHeight(String arg) throws ValidationException {
+        try {
+            Float height = Float.valueOf(arg);
+            if (height <= 0) {
+                throw new ValidationException("Рост человека должен быть больше нуля.");
+            }
+        } catch (NumberFormatException e) {
+            throw new ValidationException("Рост человека должен быть числом типа float.");
         }
-        return height;
     }
 
-    public static int validateWeight(int weight) throws ValidationException {
-        if (weight <= 0) {
-            throw new ValidationException("Вес человека должен быть больше нуля.");
+    public static void validateWeight(String arg) throws ValidationException {
+        try {
+            int weight = Integer.parseInt(arg);
+            if (weight <= 0) {
+                throw new ValidationException("Вес человека должен быть больше нуля.");
+            }
+        } catch (NumberFormatException e) {
+            throw new ValidationException("Вес человека должен быть числом типа int.");
         }
-        return weight;
     }
 
-    public static String validatePassportID (String passportID) throws ValidationException {
-        if (passportID.length() > 28) {
+    public static void validatePassportID (String arg) throws ValidationException {
+        if (arg.length() > 28) {
             throw new ValidationException("Длина паспортых данных не может быть больше 28.");
         }
-        return passportID;
     }
 }
