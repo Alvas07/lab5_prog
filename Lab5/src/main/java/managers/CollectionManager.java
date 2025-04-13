59183558a0ea8587a6abf435c6c8d451374a6924
Владислав Дядев <vladislavdyadev@MacBook-Air-Vladislav-4.local>
2025-04-14 02:19:11 +0300

    Added CollectionManager

---
+++ Lab5/src/main/java/managers/CollectionManager.java
@@ -0,0 +1,39 @@
+package managers;
+
+import data.Ticket;
+import utils.DateTimeUtils;
+
+import java.time.LocalDateTime;
+import java.util.ArrayDeque;
+
+public class CollectionManager {
+    private final ArrayDeque<Ticket> collection;
+    private final LocalDateTime initializationTime;
+    private LocalDateTime lastUpdateTime;
+
+    public CollectionManager() {
+        this.collection = new ArrayDeque<>();
+        this.initializationTime = DateTimeUtils.getStartTime();
+        this.lastUpdateTime = DateTimeUtils.getCurrentTime();
+    }
+
+    public ArrayDeque<Ticket> getCollection() {
+        return collection;
+    }
+
+    public LocalDateTime getInitializationTime() {
+        return initializationTime;
+    }
+
+    public LocalDateTime getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void updateLastModifiedTime() {
+        lastUpdateTime = DateTimeUtils.getCurrentTime();
+    }
+
+    public int getCollectionSize() {
+        return collection.size();
+    }
+}
