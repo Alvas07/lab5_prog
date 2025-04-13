aeef58b95d8f9d4d348cb20bb71448c564a1cd34
Владислав Дядев <vladislavdyadev@MacBook-Air-Vladislav-4.local>
2025-04-14 02:27:34 +0300

    Added InfoCommand

---
+++ Lab5/src/main/java/managers/commands/InfoCommand.java
@@ -0,0 +1,30 @@
+package managers.commands;
+
+import managers.CollectionManager;
+
+public class InfoCommand implements Command {
+    private final CollectionManager collectionManager;
+
+    public InfoCommand(CollectionManager collectionManager) {
+        this.collectionManager = collectionManager;
+    }
+
+    @Override
+    public void execute(String[] args) {
+        System.out.println("ИНФОРМАЦИЯ О КОЛЛЕКЦИИ");
+        System.out.println("Тип коллекции: " + collectionManager.getCollection().getClass().getSimpleName());
+        System.out.println("Количество элементов: " + collectionManager.getCollectionSize());
+        System.out.println("Дата инициализации: " + collectionManager.getInitializationTime());
+        System.out.println("Дата последнего изменения: " + collectionManager.getLastUpdateTime());
+    }
+
+    @Override
+    public String getName() {
+        return "info";
+    }
+
+    @Override
+    public String getDescription() {
+        return "вывести информацию о коллекции";
+    }
+}
