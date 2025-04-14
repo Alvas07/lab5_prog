fa918f3090212a600445e63f637d5ab1cf48c99d
Владислав Дядев <vladislavdyadev@MacBook-Air-Vladislav-4.local>
2025-04-14 10:33:22 +0300

    Added NotEnoughArgumentsException

---
+++ Lab5/src/main/java/exceptions/NotEnoughArgumentsException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class NotEnoughArgumentsException extends Exception {
+    public NotEnoughArgumentsException(String message) {
+        super("Недостаточно аргументов: " + message);
+    }
+}
