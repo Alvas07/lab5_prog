ac497e18b790c2613aecc0fe4de773a7b8322f43
Владислав Дядев <vladislavdyadev@MacBook-Air-Vladislav-4.local>
2025-04-26 18:06:56 +0300

    Added ScannerManager

---
+++ Lab5/src/main/java/managers/ScannerManager.java
@@ -0,0 +1,15 @@
+package managers;
+
+import java.util.Scanner;
+
+public class ScannerManager {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static Scanner getScanner() {
+        return scanner;
+    }
+
+    public static void setScanner(Scanner scanner) {
+        ScannerManager.scanner = scanner;
+    }
+}
