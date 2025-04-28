85351f2415547d6a1fe1a5e01a4010b1571fec54
Владислав Дядев <vladislavdyadev@MacBook-Air-Vladislav-4.local>
2025-04-28 03:04:47 +0300

    Updated TicketWriter Interface

--- Lab5/src/main/java/system/io/TicketWriter.java
+++ Lab5/src/main/java/system/io/TicketWriter.java
@@ -3,10 +3,10 @@ package system.io;
 import data.Ticket;
 import exceptions.FileWriteException;
 
-import java.util.List;
+import java.util.Collection;
 
 public interface TicketWriter {
-    void writeTicketsToFile(String fileName, List<Ticket> tickets) throws FileWriteException;
+    void writeTicketsToFile(String fileName, Collection<Ticket> tickets) throws FileWriteException;
 
     boolean canWrite(String fileName);
 }
