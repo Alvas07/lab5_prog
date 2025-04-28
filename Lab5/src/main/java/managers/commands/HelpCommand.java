8a82d529a48831bc7ee521d7bbdc3b76407d90b2
Владислав Дядев <vladislavdyadev@MacBook-Air-Vladislav-4.local>
2025-04-28 09:29:19 +0300

    Added HelpCommand

---
+++ Lab5/src/main/java/managers/commands/HelpCommand.java
@@ -0,0 +1,35 @@
+package managers.commands;
+
+import exceptions.CommandExecuteException;
+import managers.CollectionManager;
+import managers.CommandManager;
+
+import java.util.LinkedHashMap;
+
+public class HelpCommand implements Command {
+    private final CollectionManager collectionManager;
+
+    public HelpCommand(CollectionManager collectionManager) {
+        this.collectionManager = collectionManager;
+    }
+
+    @Override
+    public void execute(String[] args) throws CommandExecuteException {
+        CommandManager commandManager = new CommandManager(collectionManager);
+        LinkedHashMap<String, Command> commandList = commandManager.getCommandList();
+        for (String commandName : commandList.keySet()) {
+            Command command = commandList.get(commandName);
+            System.out.println(command.getName() + " - " + command.getDescription());
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "help";
+    }
+
+    @Override
+    public String getDescription() {
+        return "вывести справку по доступным командам";
+    }
+}
