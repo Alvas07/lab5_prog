9e4c548fac7661b072685a71487824dd498df8eb
Владислав Дядев <vladislavdyadev@MacBook-Air-Vladislav-4.local>
2025-05-03 00:45:28 +0300

    Updated ValidationException to ObjectCreationException

--- Lab5/src/main/java/exceptions/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class ValidationException extends Exception {
-    public ValidationException(String message) {
-        super("Ошибка при валидации данных: " + message);
-    }
-}
